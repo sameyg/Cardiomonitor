3bd0658fd4de67155b334baaa658d5779605496e
Sam Amey-Gonzalez <samamey95@gmail.com>
2018-08-11 19:43:22 +0100

    Initial Commit

---
+++ Cardiomonitor/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Cardiomonitor
+//
+//  Created by Samantha Amey-Gonzalez on 7/7/18.
+//  Copyright © 2018 RVC. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Cardiomonitor/SecondViewController.swift
@@ -0,0 +1,26 @@
+//
+//  SecondViewController.swift
+//  Cardiomonitor
+//
+//  Created by Samantha Amey-Gonzalez on 7/7/18.
+//  Copyright © 2018 RVC. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
+
